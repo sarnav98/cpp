28b8f39657950d225180d7051de81c80e90b8a9a
sarnav98 <sarnavdutta2011@gmail.com>
2020-09-15 21:05:18 +0530

    random

---
+++ arrayadt/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+//Operation in Arrays
+
+/*
+1. Display ()
+2. Add / Append
+3. Insert
+4. Delete
+5. Search
+6. Get(index)
+7. Set (index,x)
+8. Max() / Min ()
+9. Reverse ()
+10. Shift/Rotate()
+*/
+
---
+++ insertarr/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+// Traverse of the array
+int main()
+{
+    /*
+    int a[50],size;
+    cout << "Enter size of the array \n";
+    cin >> size;
+    cout << "Enter the elements of the array \n";
+    for (int i=0;i<size;i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i=0; i<size; i++)
+    {
+        cout << " " << a[i];
+    }
+    return 0;
+
+    //Addition / Append
+    a[length] = x;
+    length++;
+    */
+}
